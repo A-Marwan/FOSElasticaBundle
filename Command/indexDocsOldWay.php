c1ef93bb26aed2b103c86fa38df6d845b68b2c76
MaЯИaN <amamou.marwan@gmail.com>
2015-08-06 13:53:35 +0200

    oops

--- Command/indexDocsOldWay.php
+++ Command/indexDocsOldWay.php
@@ -1,12 +1,15 @@
 <?php
 
 
-namespace ECD\Command;
-use Console\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Input\InputInterface;
+
+namespace FOS\ElasticaBundle\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Elastica\Query;
+use Elastica\Result;
 
 class IndexDocumentCommand extends Command
 {
